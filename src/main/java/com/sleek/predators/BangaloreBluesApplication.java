da96a355c80b8e426c1a06ef8feaa661e06c683b
Aniket Raj <info.aniketraj@gmail.com>
2023-08-30 17:39:09 +0530

    spring boot inital dump

---
+++ src/main/java/com/sleek/predators/BangaloreBluesApplication.java
@@ -0,0 +1,13 @@
+package com.sleek.predators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BangaloreBluesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BangaloreBluesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sleek/predators/BangaloreBluesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sleek.predators;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BangaloreBluesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
