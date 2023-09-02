781cf35fee5171a9d5b364d82d8295d2f59ad442
Aniket Raj <39947497+aniketraj1947@users.noreply.github.com>
2023-09-03 01:06:26 +0530

    added api scrapper abstraction (#2)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module predators {
+    requires flow.html.components;
+    requires flow.server;
+    requires spring.aspects;
+    requires spring.boot.autoconfigure;
+    requires vaadin.flow.components.base;
+    requires vaadin.ordered.layout.flow;
+}
